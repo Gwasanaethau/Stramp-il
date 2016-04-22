325a6eb68193cf6b9b8b6bff9b99a87872661bf4
Mark Pokorny <iridium193@gmail.com>
2016-04-22 01:10:20 +0100

    New ‘Constants’ interface created containing many String constants that are referenced from various places within the package.

---
+++ Constants.java
@@ -0,0 +1,26 @@
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+package strampáil;
+
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+/**
+ * Contains many constants referred to throughout the package.
+ *
+ * @author Mark David Pokorny
+ * @version Dé hAoine, 22ú Aibreán 2016
+ * @since Dé hAoine, 22ú Aibreán 2016
+ */
+interface Constants
+{
+
+// ----------------------------------------- Constants Interface ---------------
+
+  String STOMP_VER = "accept-version:1.1\n";
+  String HOST_NAME = "host:localhost\n";
+
+// ----------------------------------------- Constants Interface ---------------
+
+} // End ‘Constants’ Interface
+
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
