0f218305ddeba2b053714c50763a3efe2c18bfbb
Mark Pokorny <iridium193@gmail.com>
2016-04-22 02:29:11 +0100

    Client can now send ‘STOMP’ and ‘CONNECT’ commands.

--- STOMPClient.java
+++ STOMPClient.java
@@ -1,8 +1,8 @@
 // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
 package strampáil;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -15,10 +15,10 @@ import java.net.UnknownHostException;
  * with version 1.1 of the STOMP protocol.
  *
  * @author Mark David Pokorny
- * @version Déardaoin, 21ú Aibreán 2016
+ * @version Dé hAoine, 22ú Aibreán 2016
  * @since Déardaoin, 21ú Aibreán 2016
  */
-public class STOMPClient
+public class STOMPClient implements Constants
 {
 
 // ------------------------------------------- STOMPClient Class ---------------
@@ -27,7 +27,9 @@ public class STOMPClient
 
   private Socket socket;
   private int port;
-  private BufferedReader receiver;
+  private boolean tcpConnected;
+  private InputStream receiver;
+  private OutputStream transmitter;
 
 // ------------------------------------------- STOMPClient Class ---------------
 
@@ -44,12 +46,14 @@ public class STOMPClient
       "Winding-up client and connecting to server on port: \033[36m" +
       port + "\033[0m");
     this.port = port;
+    tcpConnected = false;
 
     try
     {
       socket = new Socket("localhost", port);
-      receiver = new BufferedReader(
-        new InputStreamReader(socket.getInputStream()));
+      receiver = socket.getInputStream();
+      transmitter = socket.getOutputStream();
+      tcpConnected = true;
     } // End try
 
     catch (UnknownHostException uhe)
@@ -76,35 +80,139 @@ public class STOMPClient
 // ------------------------------------------- STOMPClient Class ---------------
 
   /**
-   * Instructs the client to disconnect from the server and shut itself down.
+   * Sends a STOMP 1.0 <code>CONNECT</code> command to the server.
+   *
+   * @param login The login name to send to the server
+   * (can be <code>null</code>) if no login required.
+   * @param password The password to be sent along with the login name
+   * (can be <code>null</code>) if no login required.
+   * @see #stomp(String, String)
    */
-  public String receive()
+  public void connect(String login, String password)
   {
 
-    StringBuilder partialMessage = new StringBuilder();
+    this.command("CONNECT", login, password);
+
+  } // End ‘connect(String, String)’ Method
+
+// ------------------------------------------- STOMPClient Class ---------------
+
+  /**
+   * Sends a STOMP 1.0 <code>CONNECT</code> command to the server without any
+   * login details.
+   *
+   * @see #connect(String, String)
+   */
+  public void connect()
+  {
+
+    this.connect(null, null);
+
+  } // End ‘connect()’ Method
+
+// ------------------------------------------- STOMPClient Class ---------------
+
+  /**
+   * Sends a STOMP 1.1 <code>STOMP</code>
+   * (formerly <code>CONNECT</code>) command to the server.
+   *
+   * @param login The login name to send to the server
+   * (can be <code>null</code>) if no login required.
+   * @param password The password to be sent along with the login name
+   * (can be <code>null</code>) if no login required.
+   */
+  public void stomp(String login, String password)
+  {
+
+    this.command("STOMP", login, password);
+
+  } // End ‘stomp(String, String)’ Method
+
+// ------------------------------------------- STOMPClient Class ---------------
+
+  /**
+   * Sends a STOMP 1.1 <code>STOMP</code>
+   * (formerly <code>CONNECT</code>) command to the server without any
+   * login details.
+   *
+   * @see #stomp(String, String)
+   */
+  public void stomp()
+  {
+
+    this.stomp(null, null);
+
+  } // End ‘stomp()’ Method
+
+// ------------------------------------------- STOMPClient Class ---------------
+
+  private void command(String command, String login, String password)
+  {
+
+    if (!tcpConnected)
+    {
+
+      System.err.println(PROG_NAME + ": \033[31mCannot send" +
+        " \033[1m" + command + "\033[0;31m command due to no TCP connection" +
+        ". Ensure that you can connect to the server with TCP first!\033[0m");
+      return;
+
+    } // End if
+
+    StringBuilder stompFrame = new StringBuilder(
+      command + "\n" + /*STOMP_VER +*/ HOST_NAME);
+
+    if (login != null && password != null)
+      stompFrame.append("login:" + login + "\npasscode:" + password + "\n");
+
+    stompFrame.append("\n\0");
 
     try
     {
-      while (true)
-      {
-        char character = (char) receiver.read();
-
-        if (character == '\0')
-          break;
-
-        partialMessage.append(character);
-      } // End while
+      transmitter.write(stompFrame.toString().getBytes());
     } // End try
 
     catch (IOException ioe)
     {
-      System.err.println(PROG_NAME + ": " +
-        "\033[31mUnable to parse message.\033[0m");
+      System.err.println(PROG_NAME + ": \033[31mCannot send" +
+        "\033[1m" + command + "\033[0;31m command.\033[0m");
+      this.close();
     } // End ‘IOException’ catch
 
-    return partialMessage.toString();
+  } // End ‘command(String, String, String)’ Method
 
-  } // End ‘receive()’ Method
+// ------------------------------------------- STOMPClient Class ---------------
+
+  // /**
+  //  * Instructs the client to disconnect from the server and shut itself down.
+  //  */
+  // public String receive()
+  // {
+  //
+  //   StringBuilder partialMessage = new StringBuilder();
+  //
+  //   try
+  //   {
+  //     while (true)
+  //     {
+  //       char character = (char) receiver.read();
+  //
+  //       if (character == '\0')
+  //         break;
+  //
+  //       partialMessage.append(character);
+  //     } // End while
+  //   } // End try
+  //
+  //   catch (IOException ioe)
+  //   {
+  //     System.err.println(PROG_NAME + ": " +
+  //       "\033[31mUnable to parse message.\033[0m");
+  //   } // End ‘IOException’ catch
+  //
+  //   return partialMessage.toString();
+  //
+  // } // End ‘receive()’ Method
 
 // ------------------------------------------- STOMPClient Class ---------------
 
@@ -121,6 +229,7 @@ public class STOMPClient
       try
       {
         socket.close();
+        tcpConnected = false;
       } // End try
 
       catch (IOException ioe)
@@ -146,8 +255,9 @@ public class STOMPClient
   public static void main(String args[])
   {
 
-    STOMPClient client = new STOMPClient(50005);
-    System.out.println("Message: " + client.receive());
+    STOMPClient client = new STOMPClient(51515);
+    client.connect();
+    //client.stomp();
     client.close();
 
   } // End ‘main(String[] args)’ Method
